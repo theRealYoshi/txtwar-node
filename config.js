1dfcd9215c8d5c19ee243275446381e003dbbbb4
Yoshi <hiroluk@gmail.com>
2015-12-24 13:14:33 -0800

    adding amqp

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   database: process.env.MONGO_URI || 'localhost',
-  rabbit_url: process.env.CLOUDAMQP_URL || 'amqp://localhost',
+  rabbit_url: process.env.CLOUDAMQP_URL || 'localhost',
 
   thrifty: bool(process.env.THRIFTY) || false,                    // Web process also executes job queue?
   view_cache: bool(process.env.VIEW_CACHE) || true              // Cache rendered views?
