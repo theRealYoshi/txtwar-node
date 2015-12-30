4c50a52b9da6210d9e097cc820f3a8578452e9ff
Yoshi <hiroluk@gmail.com>
2015-12-30 01:01:05 -0800

    updated .gitignore

--- secrets.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  twilio: {
-    sid: process.env.TWILIO_SID || 'AC2b10c047fd90d4d0c6455d223f33f8d5',
-    token: process.env.TWILIO_TOKEN || '5ce83d92939d9515db965143bc113b78',
-    number: process.env.TWILIO_NUMBER || "+16503991523"
-  }
-}
